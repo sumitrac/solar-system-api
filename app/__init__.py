34190fe90c3bd1539a12391cdeb7396adee381ff
lileekoi <70069306+lileekoi@users.noreply.github.com>
2021-05-01 20:57:47 -0700

    livecode modified files

--- app/__init__.py
+++ app/__init__.py
@@ -9,9 +9,15 @@ def create_app(test_config=None):
     app = Flask(__name__)
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:postgres@localhost:5432/solar_system_development'
+    
     from app.models.planet import Planet
 
     db.init_app(app)
     migrate.init_app(app, db)
+    
+    from .routes import planet_bp
+    app.register_blueprint(planet_bp)
+
+
     return app
 
--- app/routes.py
+++ app/routes.py
@@ -1,2 +1,52 @@
-from flask import Blueprint
+from app import db
+from app.models.planet import Planet
+from flask import request, Blueprint, make_response, jsonify
 
+planets_bp = Blueprint("planets", __name__, url_prefix="/planets")
+
+
+
+@planets_bp.route("", methods=["POST"])
+def add_planets():
+    request_body = request.get_json()
+    new_planet = Planet(name=request_body["name"],
+                    description=request_body["description"])
+
+    db.session.add(new_planet)
+    db.session.commit()
+
+    return make_response(f"Planet {new_planet.name} successfully created", 201)
+
+
+@planets_bp.route("", methods=["GET", "POST"])
+def handle_planets():
+    if request.method == "GET":
+        planets = Planet.query.all()
+        planets_response = []
+        for planet in planets:
+            planet_response.append({
+                "id": planet.id,
+                "name": planet.name,
+                "description": planet.description
+            })
+        return jsonify(planet_response)
+    elif request.method == "POST":
+        request_body = request.get_json()
+        new_planet = Planet(name=request_body["name"],
+                        description=request_body["description"])
+
+
+        db.session.add(new_planet)     
+        db.session.commit() 
+        return make_response(f"Planet {new_planet.name} successfully created", 201)
+
+
+@planets_bp.route("/<planet_id>", methods=["GET"])
+def handle_planets(planet_id):
+    planets = Planet.query.get(planet_id)
+
+    return {
+        "id": planet.id,
+        "name": planet.name,
+        "description": planet.description
+    }
