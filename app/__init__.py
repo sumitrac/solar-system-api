047b66b48129e4107f9f4923ca6147e0ec3c2c71
lileekoi <70069306+lileekoi@users.noreply.github.com>
2021-05-01 21:00:49 -0700

    livecode modified files attempt 3

--- app/__init__.py
+++ app/__init__.py
@@ -9,18 +9,13 @@ def create_app(test_config=None):
     app = Flask(__name__)
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:postgres@localhost:5432/solar_system_development'
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> dbb1515f123f3ae19e82a2a93370d5abb04d8b6e
+
     
     from app.models.planet import Planet
 
-    
-=======
+
     from app.models.planet import Planet
 
->>>>>>> 01b1c1f83f145fe269ccc58f1314086f928e1e35
     db.init_app(app)
     migrate.init_app(app, db)
     
