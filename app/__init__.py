a06cc3eaa99a926d4f325a5da9f9f0bb34f8333d
marielaxcruz <marielacrtzcruz@gmail.com>
2021-05-01 20:09:37 -0700

    switched drivers

--- app/__init__.py
+++ app/__init__.py
@@ -10,6 +10,9 @@ def create_app(test_config=None):
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:postgres@localhost:5432/solar_system_development'
     
+    from app.models.planet import Planet
+
+    
     db.init_app(app)
     migrate.init_app(app, db)
     
