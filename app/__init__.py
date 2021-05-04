b61d2061312db38de1b2465470ed0b48d46f1141
Sumitra <sumitrac2008@gmail.com>
2021-05-04 11:58:20 -0700

    app.config match .env

--- app/__init__.py
+++ app/__init__.py
@@ -10,17 +10,17 @@ load_dotenv()
 
 def create_app(test_config=None):
     app = Flask(__name__)
-   
     
+#these app.config should match .env 
     if not test_config:
         app.config['TRACK_MODIFICATIONS'] = False
-        app.config['SQLALCHEMY_DATABASE_solar_system_development'] = os.environ.get(
-            "SQLALCHEMY_DATABASE_solar_system_development")
+        app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
+            "SQLALCHEMY_DATABASE_URI")
     else:
         app.config["TESTING"] = True
         app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-        app.config["SQLALCHEMY_DATABASE_solar_system_development"] = os.environ.get(
-            "SQLALCHEMY_TEST_DATABASE_solar_system_test")
+        app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
+            "SQLALCHEMY_TEST_DATABASE_URI")
 
     db.init_app(app)
     migrate.init_app(app, db)
